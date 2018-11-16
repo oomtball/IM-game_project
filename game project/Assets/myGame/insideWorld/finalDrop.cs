c9d727cb546ae3cdd261c30705eb71aafbcfdc6f
oomtball <welly0615440@gmail.com>
2018-11-16 16:22:38 +0800

    column

---
+++ game project/Assets/myGame/insideWorld/finalDrop.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class finalDrop : MonoBehaviour
+{
+
+    public static Flowchart talkFlowchart;
+    public static Flowchart flowchartManager;
+    public string onMouseDown;
+    public Transform player;
+
+    void Awake()
+    {
+        flowchartManager = GameObject.FindGameObjectWithTag("flowchartController").GetComponent<Flowchart>();
+        talkFlowchart = GameObject.FindGameObjectWithTag("talkFlowchart").GetComponent<Flowchart>();
+    }
+
+    public static bool isTalking
+    {
+        get { return flowchartManager.GetBooleanVariable("talking"); }
+    }
+
+    public static bool isChoosing
+    {
+        get { return flowchartManager.GetBooleanVariable("choosing"); }
+    }
+
+    public static bool elecFixed
+    {
+        get { return talkFlowchart.GetBooleanVariable("elecFixed"); }
+    }
+
+    private void OnMouseDown()
+    {
+        float dist = Vector3.Distance(player.position, transform.position);
+        if (dist <= 15 && !isTalking)
+        {
+            if (!elecFixed)
+            {
+                talkFlowchart.ExecuteBlock(talkFlowchart.FindBlock("noReaction"));
+            }
+            else
+            {
+                Block targetBlock = talkFlowchart.FindBlock(onMouseDown);
+                talkFlowchart.ExecuteBlock(targetBlock);
+            }
+        }
+    }
+}
