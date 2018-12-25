4bd38155bf8253157f44698fe20f8c8c558632e4
oomtball <welly0615440@gmail.com>
2018-12-26 04:37:37 +0800

    ya

---
+++ game project/Assets/goToCH1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using UnityEngine.SceneManagement;
+
+public class goToCH1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static Flowchart talkFlowchart;
+    public static Flowchart flowchartManager;
+    private void Start()
+    {
+        flowchartManager = GameObject.FindGameObjectWithTag("flowchartController").GetComponent<Flowchart>();
+        talkFlowchart = GameObject.FindGameObjectWithTag("talkFlowchart").GetComponent<Flowchart>();
+    }
+    public static bool go
+    {
+        get { return talkFlowchart.GetBooleanVariable("go"); }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (go){
+            SceneManager.LoadScene("CH1");
+        }
+    }
+}
